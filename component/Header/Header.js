df5339528a1262ff15d26336ebff7f63d0741e85
Tuyeenf <phamvantuyen1110@gmail.com>
2025-08-07 02:41:36 +0700

    fixing component js file do not run on live server

--- component/Header/Header.js
+++ component/Header/Header.js
@@ -1,21 +1,7 @@
-// const account_wrapper = document.getElementById('account_wrapper');
-// const account_dropdown = document.getElementById('account_dropdown');
-
-// document.addEventListener('click', (e) => {
-//   if (account_wrapper.contains(e.target)) {
-//     account_wrapper.classList.toggle('show');
-//   } else {
-//     account_wrapper.classList.remove('show');
-//   }
-// });
-
-
-// account_dropdown.addEventListener('click', (e) => {
-//   e.stopPropagation();
-// });
 
 
 
+//dropdown của account trên tablet và mobile
 export function setupAccountDropdown() {
   const account_wrapper = document.getElementById('account_wrapper');
   const account_dropdown = document.getElementById('account_dropdown');
@@ -37,6 +23,8 @@ export function setupAccountDropdown() {
 };
 
 
+
+// dropdown của các lựa chọn header khi màn hình tablet hoặc mobile
 export function setupDropDownBottomHeader() {
   const bot_header_dropdown_wrapper = document.getElementById('bot_header_dropdown_wrapper');
   const bot_header_dropdown = document.getElementById('bot_header_dropdown');
@@ -56,3 +44,17 @@ export function setupDropDownBottomHeader() {
     e.stopPropagation();
   });
 }
+
+
+
+
+// đổi logo khi màn to
+export function changeLOGO() {
+  if (window.matchMedia("(min-width: 1024px)").matches) {
+    document.getElementById("logo").src = "../../src/photo/logo/big-logo.png";
+
+  } else {
+    document.getElementById("logo").src = "../../src/photo/logo/logo.png";
+  }
+}
+console.log("changeLOGO called")
--- src/asset/js/main.js
+++ src/asset/js/main.js
@@ -1,10 +1,29 @@
 
 
 //Component
-import { setupAccountDropdown , setupDropDownBottomHeader } from '../../../component/Header/Header.js';
+import { setupAccountDropdown , setupDropDownBottomHeader, changeLOGO } from '../../../component/Header/Header.js';
 if (document.getElementById('account_wrapper')) {
   setupAccountDropdown();
 }
 if (document.getElementById('bot_header_dropdown_wrapper')) {
   setupDropDownBottomHeader();
 }
+window.addEventListener("resize", changeLOGO);
+
+console.log("changeLOGO called")
+
+
+// export async function loadComponent(id, htmlPath, jsPath) {
+//   const container = document.getElementById(id);
+//   const html = await fetch(htmlPath).then(res => res.text());
+//   container.innerHTML = html;
+
+//   if (jsPath) {
+//     const module = await import(jsPath);
+//     if (typeof module.setupHeader === 'function') {
+//       module.setupHeader();
+//     }
+//   }
+// }
+
+
