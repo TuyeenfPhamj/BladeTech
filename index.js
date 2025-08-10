b080c92d3905e1d6d5847256eff871668e2fcd41
Tuyeenf <phamvantuyen1110@gmail.com>
2025-08-11 03:15:56 +0700

    Category nav v1

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { loadComponent } from './src/asset/js/main.js';
 loadComponent('header-placeholder', '/Component/Header/Header.html','/Component/Header/Header.js');
 
+loadComponent('category-placeholder','/Component/CategoryNav/Category.html', '/Component/CategoryNav/Category.js')
+
 loadComponent('footer-placeholder','/Component/Footer/Footer.html', '/Component/Footer/Footer.js')
